a5800613ea423bd8de4004cb75f10bca758805fc
Kevin Snyder <kevin.snyder@certara.com>
2026-04-12 14:42:55 -0400

    added SEX to get_lb_score when score_in_list_format = F

--- test_script.R
+++ test_script.R
@@ -6,8 +6,32 @@ devtools::load_all()
 
 study_dir <- "sample_data/35449"
 
-get_doses(xpt_dir = study_dir)
-get_compile_data(xpt_dir = study_dir)
-get_bw_score(xpt_dir = study_dir)
-get_all_score(xpt_dir = study_dir, domain = c('lb', 'bw', 'mi'))
-get_treatment_group(xpt_dir = study_dir)
+Doses <- get_doses(xpt_dir = study_dir)
+treatmentGroups <- get_treatment_group(xpt_dir = study_dir)
+
+Compiled_Data <- get_compile_data(xpt_dir = study_dir)
+
+BWscores <- get_bw_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = F)
+BWscoresList <- get_bw_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = T)
+
+LBscores <- get_lb_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = F)
+LBscoresList <- get_lb_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = T)
+
+MIscores <- get_mi_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = F)
+MIscoresList <- get_mi_score(xpt_dir = study_dir, 
+                         master_CompileData = Compiled_Data,
+                         score_in_list_format = T)
+
+Scores <- get_all_score(xpt_dir = study_dir, domain = c('lb', 'bw', 'mi'))
+
+
